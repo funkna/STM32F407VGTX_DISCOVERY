348eef4234c0779ddfc6be566002564f5faf37fb
Nathan Funk <funk.na@gmail.com>
2024-02-16 22:41:09 -0700

    Add main.c description

--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
-// Includes ---------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// Author: Nathan Funk
+// Date: February 2024
+//
+// This project contains the source code for the STM32F407VGT6 Discovery Board, done in step with
+// the Master MCU Udemy Course:
+// https://www.udemy.com/course/mastering-microcontroller-with-peripheral-driver-development
+//
+// Documentation:
+// https://www.st.com/en/microcontrollers-microprocessors/stm32f407vg.html
+// https://www.st.com/en/evaluation-tools/stm32f4discovery.html
+// https://www.st.com/resource/en/programming_manual/pm0214-stm32-cortexm4-mcus-and-mpus-programming-manual-stmicroelectronics.pdf
+// ------------------------------------------------------------------------------------------------
+
 // Drivers
 #include "driver/cortex-m4/nvic/nvic_driver.h"
 
